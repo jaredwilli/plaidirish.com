513b6c4b24e32fbb3291f69c75c05b2603febb42
Jared Williams <jaredwilli@gmail.com>
2011-06-03 04:07:00 -0400

    keydown token this, what is it

--- plaidirish.js
+++ plaidirish.js
@@ -1,84 +1,86 @@
-var plaid_count = 0;
+(function() {
 
-function add_plaid() {
-	plaid_count += 1;
-	var plaid_url = 'http://www.plaidirish.com/';
-	var div = document.createElement('div');
-	var img = document.createElement('img');
-	var css = document.createElement('link');
-	var head = document.head;
-	var body = document.body;
-	var height = window.height;
-	var width = window.width;
-	var type = 'px';
-	var now = new Date();
-	var time = now.getTime();
-	var random = Math.random();
-	var ease = 'all .1s linear';
+	var plaid_count = 0;
 
-	div.style.position = 'fixed';
-	
-	div.onclick = add_plaid;
-	div.style.zIndex = 10;
-	div.style.outline = 0;
-	
-	if( plaid_count == 15 ) {
-		div.style.top = Math.max( 0, Math.round( (height - 530)/2 ) )  + 'px';
-		div.style.left = Math.round( (width - 530)/2 ) + 'px';
-		div.style.zIndex = 1000;
-	} else {
-		if( type == 'px' ) {
-			div.style.top = Math.round( height * (random * 0.75)) + type;
+	function add_plaid() {
+		plaid_count += 1;
+		var plaid_url = 'http://www.plaidirish.com/';
+		var div = document.createElement('div');
+		var img = document.createElement('img');
+		var css = document.createElement('link');
+		var head = document.head;
+		var body = document.body;
+		var height = window.height;
+		var width = window.width;
+		var type = 'px';
+		var now = new Date();
+		var time = now.getTime();
+		var random = Math.random();
+		var ease = 'all .1s linear';
+
+		div.style.position = 'fixed';
+		
+		div.onclick = add_plaid;
+		div.style.zIndex = 10;
+		div.style.outline = 0;
+		
+		if( plaid_count == 15 ) {
+			div.style.top = Math.max( 0, Math.round( (height - 530)/2 ) )  + 'px';
+			div.style.left = Math.round( (width - 530)/2 ) + 'px';
+			div.style.zIndex = 1000;
 		} else {
-			div.style.top = 0;
+			if( type == 'px' ) {
+				div.style.top = Math.round( height * (random * 0.75)) + type;
+			} else {
+				div.style.top = 0;
+			}
+			div.style.left = Math.round( random * 90 ) + '%';
 		}
-		div.style.left = Math.round( random * 90 ) + '%';
+		
+		if( plaid_count == 15 ) { time = 0; }
+
+		img.setAttribute('src', plaid_url + 'plaid.php?r=' + time + '&url=' + document.location.href);
+
+		div.style.transition = ease;
+		div.style.WebkitTransition = ease;
+		div.style.WebkitTransform = 'rotate(1deg) scale(1.01,1.01)';
+
+		div.onmouseover = function() {
+			var size = 1 + Math.round(random * 10)/100;
+			var angle = Math.round(random * 20-10);
+			var result = 'rotate('+angle+'deg) scale('+size+', '+size+')';
+			
+			this.style.transform = result;
+			this.style.WebkitTransform = result;
+		};
+
+		div.onmouseout = function() {
+			var size = 0.9 + Math.round(random * 10)/100;
+			var angle = Math.round(random * 6 - 3);
+			var result = 'rotate('+angle+'deg) scale('+size+', '+size+')';
+		
+			this.style.transform = result;	
+			this.style.WebkitTransform = result;
+		};
+
+		body.appendChild(div);
+		div.appendChild(img);	
+		
+		// Add stylesheet.
+		if (plaid_count == 5) {
+			var plaidCss = document.querySelector('#_plaid_css');
+			
+			if( !plaidCss ) {
+				css.id = '_plaid_css';
+				css.type = 'text/css';
+				css.rel = 'stylesheet';
+				css.href = 'http://plaidirish.com/plaid.css';
+				css.media = 'screen';
+				head.appendChild(css);
+			}
+			plaid_replace();
+		}	
 	}
-	
-	if( plaid_count == 15 ) { time = 0; }
-
-	img.setAttribute('src', plaid_url + 'plaid.php?r=' + time + '&url=' + document.location.href);
-
-	div.style.transition = ease;
-	div.style.WebkitTransition = ease;
-	div.style.WebkitTransform = 'rotate(1deg) scale(1.01,1.01)';
-
-	div.onmouseover = function() {
-		var size = 1 + Math.round(random * 10)/100;
-		var angle = Math.round(random * 20-10);
-		var result = 'rotate('+angle+'deg) scale('+size+', '+size+')';
-		
-		this.style.transform = result;
-		this.style.WebkitTransform = result;
-	};
-
-	div.onmouseout = function() {
-		var size = 0.9 + Math.round(random * 10)/100;
-		var angle = Math.round(random * 6 - 3);
-		var result = 'rotate('+angle+'deg) scale('+size+', '+size+')';
-	
-		this.style.transform = result;	
-		this.style.WebkitTransform = result;
-	};
-
-	body.appendChild(div);
-	div.appendChild(img);	
-	
-	// Add stylesheet.
-	if (plaid_count == 5) {
-		var plaidCss = document.querySelector('#_plaid_css');
-		
-		if( !plaidCss ) {
-			css.id = '_plaid_css';
-			css.type = 'text/css';
-			css.rel = 'stylesheet';
-			css.href = 'http://plaidirish.com/plaid.css';
-			css.media = 'screen';
-			head.appendChild(css);
-		}
-		plaid_replace();
-	}	
-}
 
 	function plaid_replace() {
 		var hx = document.querySelector('h1,h2,h3,h4,h5,h6'), 
@@ -91,6 +93,7 @@ function add_plaid() {
 		}
 		hx -= 1;
 	}
+})();
 
 
 /**
